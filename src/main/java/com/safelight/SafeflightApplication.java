fd9551b68a018feab7a641a48d64c3eae0a30c74
harrishdhaithya <harrishdhaithya@gmail.com>
2026-01-31 17:52:00 +0000

    log4j changes

--- src/main/java/com/safelight/SafeflightApplication.java
+++ src/main/java/com/safelight/SafeflightApplication.java
@@ -1,13 +1,15 @@
 package com.safelight;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SafeflightApplication {
-
+    private static final Logger logger = LogManager.getLogger(SafeflightApplication.class);
 	public static void main(String[] args) {
-        System.out.println("Starting Safeflight Application");
+        logger.info("Starting Safeflight Application");
 		SpringApplication.run(SafeflightApplication.class, args);
 	}
 
